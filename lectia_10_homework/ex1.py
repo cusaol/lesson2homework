8199a5e70500952ecfecfcd9bc7d9312044f06eb
cusaol <sandacurcubet66@gmail.com>
2023-06-21 18:35:00 +0300

    exercitii

--- lectia_10_homework/ex1.py
+++ lectia_10_homework/ex1.py
@@ -4,3 +4,12 @@ Calculati de cate ori fiecare cuvant a fost folosit.
 Afisati informatia.
 """
 
+import string
+text = input('Introdu o bucata de text: ')
+for punct in list(string.punctuation) + list(string.digits):
+    text = text.replace(punct, '')
+
+toate_cuvintele = text.split(" ")
+
+dictionar_cuvinte = dict()
+print(dictionar_cuvinte)
