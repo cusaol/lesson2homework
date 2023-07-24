956963df8f0f743f62a63ab2441b529695b87a04
cusaol <sandacurcubet66@gmail.com>
2023-07-24 21:50:01 +0300

    exercitii

---
+++ lectia_22_homework/ex1.py
@@ -0,0 +1,19 @@
+"""
+Creați clasele enumerate mai jos. Utilizați moștenirea.
+Pentru toate proprietățile, utilizați sublinieri în interiorul clasei pentru a ascunde proprietățile
+(exemplu self._inner_color) și declarați metode get și set (exemplu set_inner_color).
+Creați o clasă care descrie o Formă (Shape)
+Clasa Formă ar trebui să aibă următoarele proprietăți:
+* culoarea interioară (inner color)
+* culoarea marginii (border color)
+Creați o altă clasă Cerc (Circle) care extinde clasa Formă (Shape).
+Clasa cerc ar trebui să aibă următoarele proprietăți suplimentare:
+* rază (radius)
+Creați o altă clasă Dreptunghi (Rectangle) care, de asemenea, extinde clasa Formă (Shape)
+Clasa dreptunghi ar trebui să aibă următoarele proprietăți suplimentare:
+* lățime (width)
+* lungime (length)
+Creați o altă clasă Pătrat (Square) care extinde clasa Dreptunghi (Rectangle)
+Clasa Pătrat ar trebui să se asigure că lățimea și lungimea sunt egale atunci când una dintre ele este setată.
+De exemplu, dacă setez square.set_length(4), square.get_width() ar trebui să fie și el 4.
+"""
---
+++ lectia_22_homework/ex2.py
@@ -0,0 +1,10 @@
+
+"""
+Creați o nouă clasă ListăNumere (NumbersList) care extinde clasa list.
+Clasa ListăNumere (NumbersList) ar trebui să permită doar valori numerice (int și float) să fie adăugate în listă.
+Acest lucru înseamnă că va trebui să suprascrieți funcțiile __init__, append, extend.
+Adăugați metode suplimentare după cum este descris mai jos:
+* get_sum() - returnează suma tuturor valorilor
+* get_average() - returnează valoarea medie a tuturor numerelor din listă
+"""
+
